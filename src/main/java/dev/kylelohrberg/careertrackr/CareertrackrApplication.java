c76f66a3baea9370666f6344222dd511c7a2c69e
Kyle Lohrberg <lorbs32@gmail.com>
2025-07-17 20:39:59 -0400

    Initial spring boot scaffold for careertrackr

---
+++ src/main/java/dev/kylelohrberg/careertrackr/CareertrackrApplication.java
@@ -0,0 +1,13 @@
+package dev.kylelohrberg.careertrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareertrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareertrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kylelohrberg/careertrackr/CareertrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kylelohrberg.careertrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareertrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
