2196d848f86fb8ca7131c6ba5f4a22468a010034
Kyle Lohrberg <lorbs32@gmail.com>
2025-07-18 14:23:13 -0400

    added the service layer with essential CRUD operations

---
+++ src/main/java/dev/kylelohrberg/careertrackr/service/JobApplicationService.java
@@ -0,0 +1,47 @@
+package dev.kylelohrberg.careertrackr.service;
+
+import dev.kylelohrberg.careertrackr.entity.JobApplication;
+import dev.kylelohrberg.careertrackr.repository.JobApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobApplicationService {
+
+    private final JobApplicationRepository repository;
+
+    @Autowired
+    public JobApplicationService(JobApplicationRepository repository){
+        this.repository = repository;
+    }
+
+    public List<JobApplication> getAllApplications(){
+        return repository.findAll();
+    }
+
+    public Optional<JobApplication> getApplicationById(Long id){
+        return repository.findById(id);
+    }
+
+    public JobApplication createApplication(JobApplication jobApplication) {
+        return repository.save(jobApplication);
+    }
+
+    public void deleteApplication(Long id){
+        repository.deleteById(id);
+    }
+
+    public JobApplication updateApplication(Long id, JobApplication updatedApp) {
+        return repository.findById(id)
+                .map(existing -> {
+            existing.setCompanyName(updatedApp.getCompanyName());
+            existing.setPositionTitle(updatedApp.getPositionTitle());
+            existing.setStatus(updatedApp.getStatus());
+            existing.setAppliedDate(updatedApp.getAppliedDate());
+            return repository.save(existing);
+        }).orElseThrow(() -> new RuntimeException("Job application not found"));
+    }
+}
